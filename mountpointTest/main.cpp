240aeb55bc7acb192ebcdb8b1ec600f394d09f20
ssai20 <ssairan@yandex.ru>
2022-11-06 14:00:08 +0600

    Initial Commit

---
+++ mountpointTest/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  mountpointTest
+//
+//  Created by Work on 06.11.2022.
+//  Copyright © 2022 Work. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
